bbf0312c41a5e762e8f16ffdba4f76a7c8d92147
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-03-03 23:13:33 +0100

    a

--- installationScript.sh
+++ installationScript.sh
@@ -3,17 +3,12 @@ sudo dnf install \
   rofi\
   htop\
   ranger\
+  -y
   
 sudo dnf install make automake gcc gcc-c++ kernel-devel pam-devel libxcb-devel
-
 git clone --recurse-submodules https://github.com/fairyglade/ly
-
 cd ly
-
 make
-
 make install installsystemd
-
 systemctl enable ly.service
-
 systemctl disable getty@tty2.service
