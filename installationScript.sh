08e9b81f715555fab3a24dcb4e441551c156a736
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-03-03 23:05:28 +0100

    compile ly

---
+++ installationScript.sh
@@ -0,0 +1,19 @@
+sudo dnf update -y
+sudo dnf install \
+  rofi\
+  htop\
+  ranger\
+  
+sudo dnf install make automake gcc gcc-c++ kernel-devel pam-devel libxcb-devel
+
+git clone --recurse-submodules https://github.com/fairyglade/ly
+
+cd ly
+
+make
+
+make install installsystemd
+
+systemctl enable ly.service
+
+systemctl disable getty@tty2.service
